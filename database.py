6a2cb1bc8e3875d7acd343afd2131bf52acfaee7
Dr-Alb <albsipoi1564@gmail.com>
2025-09-27 09:39:01 -0700

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Replace with your actual DB credentials
-DATABASE_URL = "mysql+mysqlconnector://root:password@localhost/inventory_db"
+#  actual DB credentials
+DATABASE_URL = ""
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
+
